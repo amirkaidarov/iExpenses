c98c12ac684c0fb8435d6dda92dc19159ba7ed1b
amirkaidarov <KaidarovAmir1441@gmail.com>
2023-01-08 12:03:30 -0500

    Initial Commit

---
+++ iExpenses/iExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpensesApp.swift
+//  iExpenses
+//
+//  Created by Амир Кайдаров on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
